860ef84de57e7ce7666a406c67388708980d627f
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-01 00:50:17 +0100

    bcrypt check

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,9 +6,18 @@ Password Hashing Module
 import bcrypt
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> bytes:
     """
     Function hashes password using bcrypt
     """
     hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Checks if hashed_password correlates with password
+    """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
