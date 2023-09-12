91a8bbcaab65e81dcbd3af52005aa934847711ee
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-12 14:29:36 +0100

    Sqlalchemy base class for users

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+ORM: SqlAlchemy
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    SQLAlchemy Class
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        return "<User(email='%s', id='%s')>" % (self.email, self.id)
