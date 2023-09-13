50e73baa0c32550a09d73e237c8b8d202d707f93
dhev-wisdom <nonsowidom62@gmail.com>
2023-09-13 19:04:27 +0100

    get reset password token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -8,6 +8,7 @@ from auth import Auth, _hash_password
 from db import DB
 from sqlalchemy.exc import NoResultFound
 from user import User
+import uuid
 
 AUTH = Auth()
 DB = DB()
@@ -53,6 +54,7 @@ def login():
     else:
         abort(401)
 
+
 @app.route("/sessions", strict_slashes=False, methods=["DELETE"])
 def logout():
     """
@@ -66,19 +68,38 @@ def logout():
         return redirect(url_for('home'))
     abort(403)
 
+
 @app.route("/profile", strict_slashes=False, methods=["GET"])
 def profile():
     """
     check if user exists (by session id) and return user email
     """
     session_id = request.cookies.get("session_id")
-    if session_id == None:
+    if not session_id:
         abort(403)
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         response = {"email": user.email}
         return jsonify(response)
     abort(403)
+
+
+@app.route("/reset_password", strict_slashes=False, methods=["POST"])
+def get_reset_password_token():
+    """
+    get reset password token
+    """
+    email = request.form.get("email")
+    if not email:
+        print("Email not logged in")
+        abort(403)
+    try:
+        user = DB.find_user_by(email=email)
+    except NoResultFound:
+        abort(403)
+    token = uuid.uuid4()
+    response = {"email": user.email, "reset_token": token}
+    return jsonify(response), 200
 
 
 if __name__ == "__main__":
