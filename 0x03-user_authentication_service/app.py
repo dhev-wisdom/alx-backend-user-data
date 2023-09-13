d4d6cc76d26ff6c428270413bc326c732878b59d
dhev-wisdom <nonsowisdom62@gmail.com>
2023-09-13 02:20:53 +0100

    request POST

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,16 +3,37 @@
 Basic Flask App
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, make_response
+from auth import Auth
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
     """home route"""
     res = {"message": "Bienvenue"}
     return jsonify(res)
+
+
+@app.route("/users", strict_slashes=False, methods=["POST"])
+def users():
+    """
+    register user with passed data
+    """
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            user = AUTH.register_user(email=email, password=password)
+            return jsonify({"email": email, "message": "user created"})
+        except ValueError:
+            res = {"message": "email already registered"}
+            status_code = 400
+            response = make_response(res, status_code)
+            return response
 
 
 if __name__ == "__main__":
