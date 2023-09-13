3bed109ae402c8cf3a8102037966f5cb6435babc
dhev-wisdom <nonsowidom62@gmail.com>
2023-09-13 23:49:00 +0100

    Solved all

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 Basic Flask App
 """
 
-from flask import Flask, jsonify, request, abort, redirect
+from flask import Flask, jsonify, request, abort, redirect, url_for
 from auth import Auth, _hash_password
 from db import DB
 from sqlalchemy.exc import NoResultFound
@@ -91,10 +91,10 @@ def get_reset_password_token():
     """
     email = request.form.get("email")
     try:
-        reset_token = AUTH.get_reset_password_token(email)
+        reset_token = AUTH.generate_reset_password_token(email)
         if not reset_token:
             abort(403)
-        response = {"email": user.email, "reset_token": token}
+        response = {"email": email, "reset_token": reset_token}
         return jsonify(response), 200
     except ValueError:
         abort(403)
@@ -105,14 +105,15 @@ def update_password():
     """
     update user password
     """
-    email, reset_token = request.form.get("email"), request.form.get("reset_token")
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
     password = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, password)
         response = {"email": email, "message": "Password updated"}
         return jsonify(response), 200
     except Exception as e:
-            abort(403)
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -1,11 +1,87 @@
 #!/usr/bin/env python3
 """
-Main file
+Module documentation
 """
-from user import User
 
-print(User.__tablename__)
+import requests
 
-for column in User.__table__.columns:
-    print("{}: {}".format(column, column.type))
 
+def register_user(email: str, password: str) -> None:
+    """querry route to register user"""
+    url = "http://127.0.0.1:5000/users"
+    payload = {"email": email, "password": password}
+    res = requests.post(url, payload)
+    assert res.status_code == 200
+
+
+def log_in_wrong_password(email: str, password: str) -> None:
+    """wrong password login"""
+    url = "http://127.0.0.1:5000/sessions"
+    payload = {"email": email, "password": password}
+    res = requests.post(url, payload)
+    assert res.status_code == 401
+
+
+def log_in(email: str, password: str) -> str:
+    """login"""
+    url = "http://127.0.0.1:5000/sessions"
+    payload = {"email": email, "password": password}
+    res = requests.post(url, payload)
+    assert res.status_code == 200
+    return res.cookies.get("session_id")
+
+
+def profile_unlogged() -> None:
+    """try to access profile when user not logged in"""
+    url = "http://127.0.0.1:5000/profile"
+    res = requests.get(url)
+    assert res.status_code == 403
+
+
+def profile_logged(session_id: str) -> None:
+    """try to access profile when user not logged in"""
+    url = "http://127.0.0.1:5000/profile"
+    res = requests.get(url, cookies={"session_id": session_id})
+    assert res.status_code == 200
+
+
+def log_out(session_id: str) -> None:
+    """logout"""
+    url = "http://127.0.0.1:5000/sessions"
+    res = requests.delete(url, cookies={"session_id": session_id})
+    assert res.status_code == 200
+
+
+def reset_password_token(email: str) -> str:
+    """reset password"""
+    url = "http://127.0.0.1:5000/reset_password"
+    payload = {"email": email}
+    res = requests.post(url, payload)
+    assert res.status_code == 200
+    return res.json().get("reset_token")
+
+
+def update_password(email: str, reset_token: str, new_password: str) -> None:
+    """reset password"""
+    url = "http://127.0.0.1:5000/reset_password"
+    payload = {"email": email, "reset_token": reset_token,
+               "new_password": new_password}
+    res = requests.put(url, payload)
+    assert res.status_code == 200
+
+
+EMAIL = "guillaume@holberton.io"
+PASSWD = "b4l0u"
+NEW_PASSWD = "t4rt1fl3tt3"
+
+
+if __name__ == "__main__":
+    register_user(EMAIL, PASSWD)
+    log_in_wrong_password(EMAIL, NEW_PASSWD)
+    profile_unlogged()
+    session_id = log_in(EMAIL, PASSWD)
+    profile_logged(session_id)
+    log_out(session_id)
+    reset_token = reset_password_token(EMAIL)
+    update_password(EMAIL, reset_token, NEW_PASSWD)
+    log_in(EMAIL, NEW_PASSWD)
